18d5b9c6b81a4b0234ff6744a30a9e2168acff44
David Conner <dconner.pro@gmail.com>
2015-05-23 10:40:26 -0400

    serving static assets from the webserver for now

--- app/src/production/java/com/voxxel/Constants.java
+++ app/src/production/java/com/voxxel/Constants.java
@@ -2,4 +2,5 @@ package com.voxxel;
 
 public class Constants {
     public final static String BASE_URL = "http://api.vox.xel.io";
+    public final static String ASSETS_URL = "http://api.vox.xel.io";
 }
