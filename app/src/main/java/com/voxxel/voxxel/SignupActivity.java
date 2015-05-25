5a950bfb6e7075c0418b1757fd29b15b888f29ef
David Conner <dconner.pro@gmail.com>
2015-05-25 16:16:30 -0400

    started implementing UserSignupTask and validation

--- app/src/main/java/com/voxxel/voxxel/SignupActivity.java
+++ app/src/main/java/com/voxxel/voxxel/SignupActivity.java
@@ -3,6 +3,7 @@ package com.voxxel.voxxel;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -58,6 +59,41 @@ public class SignupActivity extends AppCompatActivity {
     }
 
     public void attemptSignup() {
+        if (mSignupTask != null) {
+            return;
+        }
+
+        // Reset errors.
+        mEmailView.setError(null);
+        mUsernameView.setError(null);
+        mPasswordView.setError(null);
+        mPasswordConfirmView.setError(null);
+
+        // Store values at the time of the login attempt.
+        String email = mEmailView.getText().toString();
+        String username = mUsernameView.getText().toString();
+        String password = mPasswordView.getText().toString();
+        String passwordConfirm = mPasswordConfirmView.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        // Check for a valid password, if the user entered one.
+        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
+            mPasswordView.setError(getString(R.string.error_invalid_password));
+            focusView = mPasswordView;
+            cancel = true;
+        } else if (!doesPasswordMatch(password, passwordConfirm)) {
+            mPasswordView.setError(getString(R.string.error_password_confirm));
+            focusView = mPasswordConfirmView;
+            cancel = true;
+        }
+
+        if (cancel) {
+            focusView.requestFocus();
+        } else {
+            //TODO: showProgress(true);
+        }
 
     }
 
@@ -65,8 +101,12 @@ public class SignupActivity extends AppCompatActivity {
         return false;
     }
 
-    private boolean isPasswordValid(String password, String confirm) {
-        return (password.length() > 4) && (password.equals(confirm));
+    private boolean isPasswordValid(String password) {
+        return password.length() > 4;
+    }
+
+    private boolean doesPasswordMatch(String password, String confirm) {
+        return password.equals(confirm);
     }
 
     private boolean isEmailValid(String email) {
@@ -74,6 +114,19 @@ public class SignupActivity extends AppCompatActivity {
     }
 
     public class UserSignupTask extends AsyncTask<Void, Void, Boolean> {
+        //TODO: instantiate UserModel class instead
+        private final String mEmail;
+        private final String mUsername;
+        private final String mPassword;
+        private final String mPasswordConfirm;
+
+        UserSignupTask(String email, String username, String password, String passwordConfirm) {
+            mEmail = email;
+            mUsername = username;
+            mPassword = password;
+            mPasswordConfirm = passwordConfirm;
+        }
+
         @Override
         protected Boolean doInBackground(Void... params) {
             return false;
