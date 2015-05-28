aae6f50041127a9feebdccb4aa4bdeeaffb13c4d
David Conner <dconner.pro@gmail.com>
2015-05-27 23:53:25 -0400

    cleanup login

--- app/src/main/java/com/voxxel/api/AuthManager.java
+++ app/src/main/java/com/voxxel/api/AuthManager.java
@@ -17,6 +17,9 @@ public class AuthManager {
     private static AccessTokenModel token;
     private String tokenKey = "token";
     private String tokenTypeKey = "token_type";
+    private String tokenClientKey = "token_client";
+    private String tokenExpiryKey = "token_expiry";
+    private String tokenUidKey = "token_uid";
 
     private AuthManager() {
         token = null;
@@ -24,10 +27,6 @@ public class AuthManager {
 
     public static AuthManager getInstance() {
         return instance;
-    }
-
-    public static AccessTokenModel getToken() {
-        return token;
     }
 
     public void setContext(Context baseContext) {
@@ -40,15 +39,19 @@ public class AuthManager {
         token = newToken;
         prefsEditor.putString(tokenKey, token.getAccessToken()).commit();
         prefsEditor.putString(tokenTypeKey, token.getTokenType()).commit();
-        Log.i("AuthManager", token.toString());
-        Log.i("AuthManager", prefs.getString(tokenKey, ""));
-        Log.i("AuthManager", prefs.getString(tokenTypeKey, ""));
+        prefsEditor.putString(tokenClientKey, token.getClient()).commit();
+        prefsEditor.putString(tokenExpiryKey, token.getExpiry()).commit();
+        prefsEditor.putString(tokenUidKey, token.getUid()).commit();
     }
 
     public AccessTokenModel retrieveToken() {
         token = new AccessTokenModel();
         token.setAccessToken(prefs.getString(tokenKey, ""));
         token.setTokenType(prefs.getString(tokenTypeKey, ""));
+        token.setClient(prefs.getString(tokenClientKey, ""));
+        token.setExpiry(prefs.getString(tokenExpiryKey, ""));
+        token.setUid(prefs.getString(tokenUidKey, ""));
+
         return token;
     }
 
