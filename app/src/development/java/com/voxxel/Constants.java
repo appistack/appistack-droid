d8cbdaf42294e2681563243a8f21c3b3374b7936
David Conner <dconner.pro@gmail.com>
2015-05-30 13:39:31 -0400

    developing at coffeeshop, must manually change IP in 5 places
    * womp womp

--- app/src/development/java/com/voxxel/Constants.java
+++ app/src/development/java/com/voxxel/Constants.java
@@ -2,7 +2,8 @@ package com.voxxel;
 
 public class Constants {
 //    public final static String BASE_URL = "http://10.0.0.3:3000";
-    public final static String BASE_URL = "https://voxxel.herokuapp.com";
+    public final static String BASE_URL = "http://192.168.1.82:3000";
+//    public final static String BASE_URL = "https://voxxel.herokuapp.com";
     public final static String ASSETS_URL = "http://vox.xel.io";
     public final static String SIGNUP_CONFIRM_SUCCESS_URL = "http://vox.xel.io";
 }
