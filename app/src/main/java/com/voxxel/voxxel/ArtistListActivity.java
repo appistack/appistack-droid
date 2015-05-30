800f84c68199b268d59e545b86fbf149e1c39066
David Conner <dconner.pro@gmail.com>
2015-05-29 22:02:09 -0400

    started implementing artist list

--- app/src/main/java/com/voxxel/voxxel/ArtistListActivity.java
+++ app/src/main/java/com/voxxel/voxxel/ArtistListActivity.java
@@ -1,15 +1,30 @@
 package com.voxxel.voxxel;
 
 import android.app.ListActivity;
+import android.content.Context;
 import android.content.Intent;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.voxxel.Constants;
+import com.voxxel.api.AccessTokenModel;
+import com.voxxel.api.ArtistModel;
+import com.voxxel.api.ArtistService;
+import com.voxxel.api.AuthManager;
+import com.voxxel.api.ServiceGenerator;
 
 import com.voxxel.voxxel.R;
+
+import java.util.Collections;
+import java.util.List;
 
 public class ArtistListActivity extends ListActivity {
     static final String[] ARTISTS = new String[]{
@@ -24,12 +39,24 @@ public class ArtistListActivity extends ListActivity {
             "David Conner"
     };
 
+    private List<ArtistModel> artists;
+    private ArtistService artistService;
+    private AuthManager authManager = AuthManager.getInstance();
+    private AccessTokenModel accessToken = authManager.retrieveToken();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setListAdapter(new ArrayAdapter<String>(this, R.layout.list_artist, ARTISTS));
+        setContentView(R.layout.activity_artist_list);
 
-//        setContentView(R.layout.activity_artist_list);
+        if (!accessToken.isValid()) {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        } else {
+            artistService = ServiceGenerator.createService(ArtistService.class, Constants.BASE_URL, accessToken);
+            artists = artistService.getArtists();
+            setListAdapter(new ArrayAdapter<ArtistModel>(this, R.layout.list_artist, artists));
+        }
 
         ListView listView = (ListView) findViewById(R.id.listView);
 //        listView.setOnItemClickListener(new OnItemClickListener() {
@@ -62,5 +89,45 @@ public class ArtistListActivity extends ListActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public class ArtistListAdapter extends BaseAdapter {
+        private List<ArtistModel> artists = Collections.emptyList();
+        private Context context;
+
+        public void setArtists(List<ArtistModel> artists) {
+            this.artists = artists;
+            notifyDataSetChanged();
+        }
+
+        @Override
+        public int getCount() {
+            return artists.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return artists.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LinearLayout view = (convertView != null)
+                    ? (LinearLayout) convertView
+                    : (LinearLayout) getLayoutInflater().inflate(R.layout.list_artist, parent, false);
+
+            TextView textName = (TextView) view.findViewById(R.id.name);
+            textName.setText(artists.get(position).getName());
+
+            TextView textDescription = (TextView) view.findViewById(R.id.description);
+            textDescription.setText(artists.get(position).getDescription());
+
+            return view;
+        }
     }
 }
