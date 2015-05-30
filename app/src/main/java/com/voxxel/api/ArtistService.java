89e65ff545523c6d6f3f2d292d8e845419b9d54d
David Conner <dconner.pro@gmail.com>
2015-05-30 15:35:29 -0400

    ArtistService.getArtist now accepts a long

--- app/src/main/java/com/voxxel/api/ArtistService.java
+++ app/src/main/java/com/voxxel/api/ArtistService.java
@@ -11,5 +11,5 @@ public interface ArtistService {
 
     @GET("/api/v1/artists/{id}")
     @Headers("Content-Type:application/json")
-    ArtistModel getArtist(@Path("id") int id);
+    ArtistModel getArtist(@Path("id") Long id);
 }
