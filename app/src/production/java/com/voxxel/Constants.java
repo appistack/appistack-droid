561d1856d4a7f286f2b098e6f21c4399a216dd55
David Conner <dconner.pro@gmail.com>
2015-06-01 13:53:51 -0400

    updated constants with encoding and sample rate

--- app/src/production/java/com/voxxel/Constants.java
+++ app/src/production/java/com/voxxel/Constants.java
@@ -1,7 +1,13 @@
 package com.voxxel;
 
+import android.media.MediaRecorder;
+
 public class Constants {
     public final static String BASE_URL = "https://voxxel.herokuapp.com";
     public final static String ASSETS_URL = "http://vox.xel.io";
     public final static String SIGNUP_CONFIRM_SUCCESS_URL = "http://vox.xel.io";
+
+    public final static int RECORDER_AUDIO_OUTPUT = MediaRecorder.AudioEncoder.DEFAULT;
+    public final static int RECORDER_AUDIO_ENCODING = MediaRecorder.AudioEncoder.DEFAULT;
+    public final static int RECORDER_AUDIO_SAMPLE_RATE = 8000;
 }
