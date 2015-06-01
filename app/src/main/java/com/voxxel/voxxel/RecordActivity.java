719b1d6d86f8db910a045be445270ca77dd53b14
David Conner <dconner.pro@gmail.com>
2015-05-31 23:14:57 -0400

    displaying data for sounds in record activity

--- app/src/main/java/com/voxxel/voxxel/RecordActivity.java
+++ app/src/main/java/com/voxxel/voxxel/RecordActivity.java
@@ -2,9 +2,11 @@ package com.voxxel.voxxel;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 //import android.support.v7.appcompat.R;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -15,7 +17,16 @@ import android.graphics.PorterDuff.Mode;
 import android.graphics.PorterDuffXfermode;
 import android.media.MediaPlayer;
 import android.media.AudioManager;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.voxxel.api.AccessTokenModel;
+import com.voxxel.api.ArtistModel;
+import com.voxxel.api.ArtistService;
+import com.voxxel.api.AuthManager;
+import com.voxxel.api.ServiceGenerator;
+import com.voxxel.api.SoundModel;
+import com.voxxel.api.SoundService;
 import com.voxxel.voxxel.R;
 import com.voxxel.Constants;
 
@@ -24,6 +35,12 @@ public class RecordActivity extends Activity {
     private AudioManager aManager;
     private Long artistId;
     private Long soundId;
+    private SoundModel sound = new SoundModel();
+    private AuthManager authManager = AuthManager.getInstance();
+    private AccessTokenModel accessToken = authManager.retrieveToken();
+    private SoundService soundService;
+
+    private RequestSoundTask soundTask = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,9 +51,27 @@ public class RecordActivity extends Activity {
         artistId = intent.getLongExtra("artistId", -1);
         soundId = intent.getLongExtra("soundId", -1);
 
+        if (!accessToken.isValid()) {
+            Intent loginIntent = new Intent(this, LoginActivity.class);
+            startActivity(loginIntent);
+        } else {
+            soundService = ServiceGenerator.createService(SoundService.class, Constants.BASE_URL, accessToken);
+            fetchSound();
+        }
+
 //        mPlayer = MediaPlayer.create(this, R.raw.sounds.arnold.get_to_the_choppa);
 //        mPlayer = initMediaPlayer();
 //        aManager = initAudioManager();
+    }
+
+    private void fetchSound() {
+        //TODO: remove this if API request is not a result of user interaction?
+        if (soundTask != null) {
+            return;
+        }
+
+        soundTask = new RequestSoundTask(this.soundId);
+        soundTask.execute((Void) null);
     }
 
     @Override
@@ -75,5 +110,46 @@ public class RecordActivity extends Activity {
 
     public void sharePressed(View view) {
 
+    }
+
+    private void updateSoundView(SoundModel sound) {
+        TextView textName = (TextView) findViewById(R.id.name);
+        textName.setText(sound.getName());
+    }
+
+    public class RequestSoundTask extends AsyncTask<Void, Void, Boolean> {
+        private Long soundId;
+
+        RequestSoundTask(final Long soundId) {
+            this.soundId = soundId;
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            try {
+                sound = soundService.getSound(soundId);
+            } catch (Exception e) {
+                sound = new SoundModel();
+                Log.e("API Auth", e.toString());
+                return false;
+            }
+
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(final Boolean success) {
+            if (success) {
+                updateSoundView(sound);
+            } else {
+                Toast.makeText(getApplicationContext(), "Error loading artist", Toast.LENGTH_LONG).show();
+            }
+            soundTask = null;
+        }
+
+        @Override
+        protected void onCancelled() {
+            soundTask = null;
+        }
     }
 }
