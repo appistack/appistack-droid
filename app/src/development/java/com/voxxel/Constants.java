1c8508a57dfd292f5bee9666cf5b6d8005c4ea55
David Conner <dconner.pro@gmail.com>
2015-08-06 02:21:48 -0400

    updated constants

--- app/src/development/java/com/voxxel/Constants.java
+++ app/src/development/java/com/voxxel/Constants.java
@@ -6,7 +6,8 @@ import android.media.MediaRecorder;
 public class Constants {
     public final static String BASE_URL = "http://192.168.1.80:3000";
     public final static String ASSETS_URL = "http://vox.xel.io";
-    public final static String SIGNUP_CONFIRM_SUCCESS_URL = "http://vox.xel.io";
+    public final static String SIGNUP_CONFIRM_SUCCESS_URL = BASE_URL;
+    public final static String PASSWORD_RESET_SUCCESS_URL = BASE_URL + "/password/change";
 
     public final static int RECORDER_AUDIO_OUTPUT = MediaRecorder.AudioEncoder.DEFAULT; //AMR_NB //AAC
     public final static int RECORDER_AUDIO_ENCODING = AudioFormat.ENCODING_PCM_16BIT;
--- app/src/production/java/com/voxxel/Constants.java
+++ app/src/production/java/com/voxxel/Constants.java
@@ -5,7 +5,8 @@ import android.media.MediaRecorder;
 public class Constants {
     public final static String BASE_URL = "https://voxxel.herokuapp.com";
     public final static String ASSETS_URL = "http://vox.xel.io";
-    public final static String SIGNUP_CONFIRM_SUCCESS_URL = "http://vox.xel.io";
+    public final static String SIGNUP_CONFIRM_SUCCESS_URL = BASE_URL;
+    public final static String PASSWORD_RESET_SUCCESS_URL = BASE_URL + "/password/change";
 
     public final static int RECORDER_AUDIO_OUTPUT = MediaRecorder.AudioEncoder.DEFAULT;
     public final static int RECORDER_AUDIO_ENCODING = MediaRecorder.AudioEncoder.DEFAULT;
